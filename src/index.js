db73a41a98167b02d3b508870533a5c8a80aa3d1
Rahmad2830 <rahmatnurhidayat0101@gmail.com>
2025-12-15 18:53:28 +0700

    adding unmount to window

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { signal, effect } from './reactivity.js'
 import { defineScope } from './scope.js'
-import { mount } from './mount.js'
+import { mount, unmount } from './mount.js'
 
 if (typeof document !== 'undefined') {
   document.addEventListener("DOMContentLoaded", mount)
@@ -10,4 +10,5 @@ if (typeof window !== 'undefined') {
   window.signal = signal
   window.defineScope = defineScope
   window.mount = mount
+  window.mount = unmount
 }
